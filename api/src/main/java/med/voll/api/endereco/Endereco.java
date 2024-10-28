19e1db3a409c934196b593370fa21e6f81255bc0
Moroni Pereira <103333074+moroni23@users.noreply.github.com>
2024-10-28 17:39:23 -0300

    Add files via upload

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,32 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter  // Gerador automatico dos Getters e Setters
+@NoArgsConstructor // Gerador de construtor padrão, sem argumentos, exigido pelo JPA
+@AllArgsConstructor // Gerador de construtor com todos os argumentos
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+
+    }
+}
---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,39 @@
+package med.voll.api.medico;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity
+@Table(name = "medicos")
+@Getter  // Gerador automatico dos Getters e Setters
+@NoArgsConstructor // Gerador de construtor padrão, sem argumentos, exigido pelo JPA
+@AllArgsConstructor // Gerador de construtor com todos os argumentos
+@EqualsAndHashCode(of = "id") // Gerador do equals e hashcode informando somente o id
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+    @Enumerated
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
